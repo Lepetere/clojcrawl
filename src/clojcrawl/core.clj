ee010a9428680ab327811a186c67b470e187fb97
Peter Fessel <peter.fessel@live.de>
2013-06-20 12:57:02 +0200

    parser collects keywords in a vector now and sorts out stop words

--- src/clojcrawl/core.clj
+++ src/clojcrawl/core.clj
@@ -3,9 +3,17 @@
 	(:require [clojcrawl.parser :as parser])
 )
 
-(defn -main [& args]
-  (let [response1 (http-kit/get (nth args 0))]
+(defn crawl [starturl depth]
+	;(def theresultvecmap vector)
+
+	(let [response1 (http-kit/get starturl)]
 		;; Other keys :headers :body :error :opts
-		(println (parser/doParse (:body @response1)))
+		(println "ergebnis:   " (:keywords (parser/doParse (:body @response1) starturl)))
+		;; returned data structure: {:url starturl :links [] :keywords []}
 	)
+)
+
+(defn -main [& args] ;first argument has to be the start url, second the search depth
+
+	(crawl (nth args 0) (nth args 1))
 )
--- src/clojcrawl/parser.clj
+++ src/clojcrawl/parser.clj
@@ -1,48 +1,85 @@
 (ns clojcrawl.parser)
 
-(defn processLink [data]
+(def stop_words [
+	;german
+	"der", "die", "das", "einer", "eine", "ein", "und", "oder", "doch", "an", "in", "nicht", 
+	;english
+	"a", "I", "of", "so", "this", "the", "it", "you", "and", "can", "can't", "cause", "any", "got", "have", "is", "as",
+	;character
+	".", ",", ";", ":", "?", "!", "(", ")",
+	;html entities
+	"nbsp"])
+
+(defn getLinkAddress
+	[datastring]
 	;only proceed if a tag contains "href"
-	(if (.contains data "href")
-		(let [urlposition (+ (.indexOf data "href=") 6) 
-				findurlend (.indexOf data " " urlposition) 
-					urlend (if (not (< findurlend 0)) (dec findurlend) (- (count data) 1))]
+	(if (.contains datastring "href")
+		(let [urlposition (+ (.indexOf datastring "href=") 6) 
+				findurlend (.indexOf datastring " " urlposition) 
+					urlend (if (not (< findurlend 0)) (dec findurlend) (- (count datastring) 1))]
 
-					(println "    link:   " (subs data urlposition urlend))
+						(subs datastring urlposition urlend)
 		)
 	)
 )
 
-(defn processTag [data]
+(defn processTag
+	[datastring]
 	;filter out empty strings, html comments and closing tags
-	(if (and (not (clojure.string/blank? data))  (and (not= (first data) \!) (not= (first data) \/)))
-		(if (= (first data) \a) 
-			(processLink data)
+	(if (and (not (clojure.string/blank? datastring))  (and (not= (first datastring) \!) (not= (first datastring) \/)))
+		(if (= (first datastring) \a) 
+			(getLinkAddress datastring)
 		)
 	)
 )
 
-(defn processFreeText [data] 
-	(if (not (clojure.string/blank? data))
-		(println "free text:   " (clojure.string/trim-newline data))
+(defn separateKeywords
+	"returns a vector of words found in the string, all lower case"
+	[datastring]
+
+	(map #(clojure.string/lower-case %) (filter #(not (clojure.string/blank? %)) (clojure.string/split datastring #"\s|\p{Punct}")))
+)
+
+(defn throwOutStopwords
+	"returns a vector of keywords found in the string with alls stop words sorted out"
+	[datastring]
+
+	(filter 
+		(fn [k] (not (first (filter #(= k %) stop_words))))
+			(separateKeywords datastring)
 	)
 )
 
-(defn doParse [datastring]
-	
-	(loop [position 0] 
+(defn processFreeText 
+	"if string is not empty this method calls further methods to collect keywords"
+	[datastring]
+
+	(if (not (clojure.string/blank? datastring))
+			(throwOutStopwords (clojure.string/trim-newline datastring))
+	)
+)
+
+(defn collectKeywords
+	"This method counts the occurrences of all keywords and adds them to the given keyword map"
+	[datastring]
+	(comment (into (:keywords crawldata) keywords))
+)
+
+(defn doParse [datastring url]
+
+	(loop [position 0 keywords [] links []]
+		;find next tag beginning
 		(let [nextposition (.indexOf datastring "<" position)]
+			;only proceed if there is a next tag
 			(if (not= nextposition -1)
-				(do
-					(if (< position (dec nextposition))
-						(processFreeText (subs datastring position nextposition))
-					)
-					(let [oneFurtherPosition (.indexOf datastring ">" nextposition)]
-						(do
-							(processTag (subs datastring (inc nextposition) oneFurtherPosition))
-							(recur (inc oneFurtherPosition))
-						)
+				;get the next keywords, if there is space between the last and the next tag, otherwise pass nil
+				(let [keywordsvec (if (< position (dec nextposition)) (processFreeText (subs datastring position nextposition)) nil)]
+					;find the end of the tag
+					(let [oneFurtherPosition (.indexOf datastring ">" nextposition) link (processTag (subs datastring (inc nextposition) oneFurtherPosition))]
+						(recur (inc oneFurtherPosition) (into keywords keywordsvec) (conj links link))
 					)
 				)
+				(sorted-map :url url, :keywords keywords, :links links)
 			)
 		)
 	)
