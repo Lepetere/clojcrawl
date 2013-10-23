d3c53f785d0166ca2eef6d1f2064c23bdcc1f46d
Peter Fessel <peter.fessel@live.de>
2013-10-23 18:38:44 +0200

    clojure styleguide applied (at least the indendation and naming parts)

--- src/clojcrawl/core.clj
+++ src/clojcrawl/core.clj
@@ -1,57 +1,38 @@
 (ns clojcrawl.core
-	(:require [org.httpkit.client :as http-kit])
-	(:require [clojcrawl.parser :as parser])
-)
+  (:require [org.httpkit.client :as http-kit])
+  (:require [clojcrawl.parser :as parser]))
 
-(defn printReport
-	"prints out a report of a site data set"
-	[url crawldata]
-
-	(println "url:              " url)
-	(println "depth:            " (pr-str (:depth crawldata)))
-	(println "keywords found:   " (pr-str (:keywords crawldata)))
-	(println "links found:      " (pr-str (:links crawldata)))
-	(println)
-)
+(defn print-report
+  "prints out a report of a site data set"
+  [url crawldata]
+  (println "url:              " url)
+  (println "depth:            " (pr-str (:depth crawldata)))
+  (println "keywords found:   " (pr-str (:keywords crawldata)))
+  (println "links found:      " (pr-str (:links crawldata)))
+  (println))
 
 (defn crawl [starturl depth printResults]
-
-	(loop [crawlQueue (-> (clojure.lang.PersistentQueue/EMPTY) (conj {:url starturl, :depth 0})) 
-			crawldata {:amountOfUrlsCrawled 0}]		
-
-		(if (or (empty? crawlQueue) (> (:depth (first crawlQueue)) depth))
-			;if maximum depth is reached or queue is empty, stop crawling process
-			crawldata
-
-			;check if url already has been crawled
-			(if (contains? crawldata (:url (first crawlQueue)))
-				;continue loop with next url
-				(recur (pop crawlQueue) crawldata)
-
-				;else parse the url
-				(let [httpResponse (http-kit/get (:url (first crawlQueue))) 
-						siteDataSet (parser/doParse (:body @httpResponse) (:depth (first crawlQueue)))]
-
-					(do
-						;print data set if wanted
-						(if (true? printResults) (printReport (:url (first crawlQueue)) siteDataSet))
-								
-						;continue working through crawl queue
-						(recur
-							;update crawlqueue -> pop element, add new found links (as maps with the right depth value)
-							(pop (into crawlQueue (map #(sorted-map :url % :depth (inc (:depth (first crawlQueue)))) (:links siteDataSet))))
-							;put new site data set in crawldata map, increase the amount of crawled sites
-							(-> (assoc crawldata (:url (first crawlQueue)) siteDataSet) (assoc :amountOfUrlsCrawled (inc (:amountOfUrlsCrawled crawldata))))
-						)
-					)
-				)
-			)
-		)
-	)
-)
+  (loop [crawlQueue (-> (clojure.lang.PersistentQueue/EMPTY) (conj {:url starturl, :depth 0})) 
+		 crawldata {:amountOfUrlsCrawled 0}]		
+    (if (or (empty? crawlQueue) (> (:depth (first crawlQueue)) depth))
+      ;;if maximum depth is reached or queue is empty, stop crawling process
+      crawldata
+      ;;check if url already has been crawled
+      (if (contains? crawldata (:url (first crawlQueue)))
+        ;;continue loop with next url
+        (recur (pop crawlQueue) crawldata)
+        ;;else parse the url
+        (let [httpResponse (http-kit/get (:url (first crawlQueue))) 
+			  siteDataSet (parser/do-parse (:body @httpResponse) (:depth (first crawlQueue)))]
+          (do
+            ;;print data set if wanted
+            (if (true? printResults) (print-report (:url (first crawlQueue)) siteDataSet))
+            ;;continue working through crawl queue
+            (recur
+            ;;update crawlqueue -> pop element, add new found links (as maps with the right depth value)
+            (pop (into crawlQueue (map #(sorted-map :url % :depth (inc (:depth (first crawlQueue)))) (:links siteDataSet))))
+            ;;put new site data set in crawldata map, increase the amount of crawled sites
+            (-> (assoc crawldata (:url (first crawlQueue)) siteDataSet) (assoc :amountOfUrlsCrawled (inc (:amountOfUrlsCrawled crawldata)))))))))))
 
 (defn -main [& args] ;first argument has to be the start url, second the search depth
-
-	(println "Total URLs crawled:  " (:amountOfUrlsCrawled (crawl (nth args 0) (Integer/parseInt (nth args 1)) true)))
-
-)
+  (println "Total URLs crawled:  " (:amountOfUrlsCrawled (crawl (nth args 0) (Integer/parseInt (nth args 1)) true))))
--- src/clojcrawl/parser.clj
+++ src/clojcrawl/parser.clj
@@ -1,121 +1,92 @@
 (ns clojcrawl.parser)
 
 (def stop_words [
-	;german
-	"der", "die", "das", "einer", "eine", "ein", "und", "oder", "doch", "an", "in", "nicht", 
-	;english
-	"a", "I", "of", "so", "this", "the", "it", "you", "and", "can", "can't", "cause", "any", "got", "have", "is", "as",
-	;character
-	".", ",", ";", ":", "?", "!", "(", ")",
-	;html entities
-	"nbsp"])
+  ;german
+  "der" "die" "das" "einer" "eine" "ein" "und" "oder" "doch" "an" "in" "nicht" 
+  ;english
+  "a" "I" "of" "so" "this" "the" "it" "you" "and" "can" "can't" "cause" "any" "got" "have" "is" "as"
+  ;character
+  "." "" ";" ":" "?" "!" "(" ")"
+  ;html entities
+  "nbsp"])
 
-(defn getLinkAddress
-	"searches an anchor tag for a href attribute and returns its link address"
-	[datastring]
-	;only proceed if a tag contains "href"
-	(if (.contains datastring "href")
-		(let [urlposition (+ (.indexOf datastring "href=") 6) 
-				findurlend (.indexOf datastring " " (+ urlposition 3)) ;plus 3 is a workaround because href content can start with whitespace
-					urlend (if (not (or (< findurlend 0) (> findurlend (count datastring)))) (dec findurlend) (- (count datastring) 1))]
+(defn get-link-address
+  "searches an anchor tag for a href attribute and returns its link address"
+  [datastring]
+  ;only proceed if a tag contains "href"
+  (if (.contains datastring "href")
+    (let [urlposition (+ (.indexOf datastring "href=") 6) 
+          findurlend (.indexOf datastring " " (+ urlposition 3)) ;plus 3 is a workaround because href content can start with whitespace
+          urlend (if (not (or (< findurlend 0) (> findurlend (count datastring)))) (dec findurlend) (- (count datastring) 1))]
 
-						(if (not (< (- urlend urlposition) 0)) (subs datastring urlposition urlend) nil)
-		)
-	)
-)
+            (if (not (< (- urlend urlposition) 0)) (subs datastring urlposition urlend) nil))))
 
-(defn processTag
-	"takes a string containing an html tag and checks if it's an anchor tag"
-	[datastring]
-	;filter out empty strings, html comments and closing tags
-	(if (and (not (clojure.string/blank? datastring))  (and (not= (first datastring) \!) (not= (first datastring) \/)))
-		;only proceed if is an anchor tag
-		(if (= (first datastring) \a)
-			(let [linkAddress (getLinkAddress datastring)]
-				(if (not= (first linkAddress) \#)
-					(str linkAddress)
-				)
-			)
-		)
-	)
-)
+(defn process-tag
+  "takes a string containing an html tag and checks if it's an anchor tag"
+  [datastring]
+  ;filter out empty strings, html comments and closing tags
+  (if (and (not (clojure.string/blank? datastring))  (and (not= (first datastring) \!) (not= (first datastring) \/)))
+    ;only proceed if is an anchor tag
+    (if (= (first datastring) \a)
+      (let [linkAddress (get-link-address datastring)]
+        (if (not= (first linkAddress) \#)
+          (str linkAddress))))))
 
-(defn separateKeywords
-	"returns a vector of words found in the string, all lower case"
-	[datastring]
+(defn separate-keywords
+  "returns a vector of words found in the string, all lower case"
+  [datastring]
+  (map #(clojure.string/lower-case %) (filter #(not (clojure.string/blank? %)) (clojure.string/split datastring #"\s|\p{Punct}"))))
 
-	(map #(clojure.string/lower-case %) (filter #(not (clojure.string/blank? %)) (clojure.string/split datastring #"\s|\p{Punct}")))
-)
+(defn throw-out-stopwords
+  "returns a vector of keywords found in the string with alls stop words sorted out"
+  [datastring]
+  (filter 
+    (fn [k] (not (first (filter #(= k %) stop_words))))
+      (separate-keywords datastring)))
 
-(defn throwOutStopwords
-	"returns a vector of keywords found in the string with alls stop words sorted out"
-	[datastring]
+(defn process-free-text 
+  "if string is not empty this method calls further methods to collect keywords"
+  [datastring]
+  (if (not (clojure.string/blank? datastring))
+    (throw-out-stopwords (clojure.string/trim-newline datastring))))
 
-	(filter 
-		(fn [k] (not (first (filter #(= k %) stop_words))))
-			(separateKeywords datastring)
-	)
-)
+(defn collect-and-insert-keywords
+  "This method counts the occurrences of all keywords and adds them to the given keyword map"
+  [crawldataKeywordMap newFoundKeywords]
+    (loop [keywordMap crawldataKeywordMap keywordVector newFoundKeywords]
+      (if (empty? keywordVector)
+        keywordMap
+        ;keywordVector not empty, insert next keyword into map:
+        (recur
+          (let [keywordCount (get keywordMap (first keywordVector))]
+          	;(assoc map key val)
+            (assoc keywordMap (first keywordVector) (if (nil? keywordCount) 1 (inc keywordCount))))
+            (rest keywordVector)))))
 
-(defn processFreeText 
-	"if string is not empty this method calls further methods to collect keywords"
-	[datastring]
+(defn do-parse 
+  "This method separates tags and text between tags; call this method to start the parsing process"
+  [datastring depth]
+  (loop [position 0 keywords {} links #{}]
+    ;end loop if there no more tag beginnings
+      (if (and (not (nil? datastring)) (not= (.indexOf datastring "<" position) -1))
+        (let [
+          ;find next tag beginning
+          nextposition (.indexOf datastring "<" position)
+          ;find the end of the tag
+          oneFurtherPosition (.indexOf datastring ">" nextposition)]
+            (recur
+              ;increase the position of the last tag's end by one
+              (inc oneFurtherPosition)
+              ;add the next keywords, if there is space between the last and the next tag, otherwise just pass keyword map
+              (if (< position (dec nextposition))
+                (collect-and-insert-keywords keywords (process-free-text (subs datastring position nextposition)))
+                keywords)
+                ;add a potentially found link address to the link set
+                  (conj links (process-tag (subs datastring (inc nextposition) oneFurtherPosition)))))
+        ;quit loop and return hash map with the crawl data
+        (sorted-map :depth depth, :keywords keywords, :links (filter #(and (not (or (= % "") (nil? %))) (.startsWith % "http:")) links)))))
 
-	(if (not (clojure.string/blank? datastring))
-			(throwOutStopwords (clojure.string/trim-newline datastring))
-	)
-)
-
-(defn collectAndInsertKeywords
-	"This method counts the occurrences of all keywords and adds them to the given keyword map"
-	[crawldataKeywordMap newFoundKeywords]
-
-		(loop [keywordMap crawldataKeywordMap keywordVector newFoundKeywords]
-			(if (empty? keywordVector)
-				keywordMap
-				;keywordVector not empty, insert next keyword into map:
-				(recur
-					(let [keywordCount (get keywordMap (first keywordVector))]
-						(assoc keywordMap (first keywordVector) (if (nil? keywordCount) 1 (inc keywordCount))) ;(assoc map key val)
-					)
-					(rest keywordVector)
-				)
-			)
-		)
-)
-
-(defn doParse 
-
-	"This method separates tags and text between tags; call this method to start the parsing process"
-
-	[datastring depth]
-
-	(loop [position 0 keywords {} links #{}]
-
-		;end loop if there no more tag beginnings
-		(if (and (not (nil? datastring)) (not= (.indexOf datastring "<" position) -1))
-			(let [
-				;find next tag beginning
-				nextposition (.indexOf datastring "<" position)
-
-				;find the end of the tag
-				oneFurtherPosition (.indexOf datastring ">" nextposition)]
-
-							(recur
-								;increase the position of the last tag's end by one
-								(inc oneFurtherPosition)
-								;add the next keywords, if there is space between the last and the next tag, otherwise just pass keyword map
-								(if (< position (dec nextposition)) 
-									(collectAndInsertKeywords keywords (processFreeText (subs datastring position nextposition))) 
-									keywords
-								)
-								;add a potentially found link address to the link set
-								(conj links (processTag (subs datastring (inc nextposition) oneFurtherPosition)))
-							)
-						)
-
-			;quit loop and return hash map with the crawl data
-			(sorted-map :depth depth, :keywords keywords, :links (filter #(and (not (or (= % "") (nil? %))) (.startsWith % "http:")) links))
-		)
-	)
-)
+;; TO DO:
+;; Prefer higher-order functions like map to loop/recur.
+;; Prefer the use of the threading macros -> (thread-first) and ->> (thread-last) to heavy form nesting.
+;; Use lisp-case for function and variable names.
