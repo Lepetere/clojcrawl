4b066e4962cde3031373c369590c079814e789bb
Peter Fessel <peter.fessel@live.de>
2014-12-21 20:24:06 +0100

    delete old synchronous crawler code

--- src/clojcrawl/core.clj
+++ src/clojcrawl/core.clj
@@ -99,31 +99,6 @@
   [starturl maximum-depth number-of-concurrent-requests]
   (fire-request (with-meta starturl {:depth 0})))
 
-(defn crawl 
-  [starturl depth number-of-concurrent-requests print-results]
-  (println "\nnumber of concurrent requests: " number-of-concurrent-requests "\n")
-  (loop [crawlQueue (-> (clojure.lang.PersistentQueue/EMPTY) (conj {:url starturl, :depth 0})) 
-		 crawldata {:amountOfUrlsCrawled 0}]		
-    (if (or (empty? crawlQueue) (> (:depth (first crawlQueue)) depth))
-      ;;if maximum depth is reached or queue is empty, stop crawling process
-      crawldata
-      ;;check if url already has been crawled
-      (if (contains? crawldata (:url (first crawlQueue)))
-        ;;continue loop with next url
-        (recur (pop crawlQueue) crawldata)
-        ;;else parse the url
-        (let [httpResponse (http-kit/get (:url (first crawlQueue))) 
-			  siteDataSet (parser/do-parse (:body @httpResponse) (:depth (first crawlQueue)))]
-          (do
-            ;;print data set if wanted
-            (if (true? print-results) (data-sink/print-report (:url (first crawlQueue)) siteDataSet))
-            ;;continue working through crawl queue
-            (recur
-            ;;update crawlqueue -> pop element, add new found links (as maps with the right depth value)
-            (pop (into crawlQueue (map #(sorted-map :url % :depth (inc (:depth (first crawlQueue)))) (:links siteDataSet))))
-            ;;put new site data set in crawldata map, increase the amount of crawled sites
-            (-> (assoc crawldata (:url (first crawlQueue)) siteDataSet) (assoc :amountOfUrlsCrawled (inc (:amountOfUrlsCrawled crawldata)))))))))))
-
 (defn -main 
   "Call like this: lein run 'http://www.peterfessel.com' 5 20
 
@@ -131,12 +106,6 @@
 
   The third argument is the number of requests that the crawler should run concurrently. It is optional and will default to 10 if omitted."
   [& args]
-  (println "Total URLs crawled:  " 
-    (:amountOfUrlsCrawled (crawl 
-      (nth args 0) 
-      (Integer/parseInt (nth args 1))
-      (if (<= (count args) 2) number-of-concurrent-requests (Integer/parseInt (nth args 2)))
-      true)))
-  #_(binding [maximum-crawl-depth (Integer/parseInt (nth args 1))
-      number-of-concurrent-requests (if (<= (count args) 2) number-of-concurrent-requests (Integer/parseInt (nth args 2)))]
-      (launch-crawl (nth args 0) maximum-crawl-depth number-of-concurrent-requests)))
+  (binding [maximum-crawl-depth (Integer/parseInt (nth args 1))
+    number-of-concurrent-requests (if (<= (count args) 2) number-of-concurrent-requests (Integer/parseInt (nth args 2)))]
+    (launch-crawl (nth args 0) maximum-crawl-depth number-of-concurrent-requests)))
